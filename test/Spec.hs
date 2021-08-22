091b25c2753bfb76b7c5ec9f1e1b7d434b3596a7
Andreas Wachs Hjalager <ahja@itu.dk>
2021-08-22 21:33:05 +0200

    Add HUnit unit test lib and begin writing cases

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Test.HUnit
+import Args
+
+main = runTestTT tests
+
+
+tests = TestList [TestLabel "Args processor will process two args" testCaseWillProcessTwoArgs]
+
+
+testCaseWillProcessTwoArgs =
+    let args = ["a", "b"]
+        flags = Args.processArgs args
+    in assertBool $ Args.isValidConfig flags
+
+
